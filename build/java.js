5c8190e0d0a59725165980fc6dc9bb13c693ee40
Felipe Canasto <estudiante123@Multimedia-4.local>
2016-08-17 10:58:55 -0500

    Se cuadro el Header, portada con background y se toco el javas

--- build/java.js
+++ build/java.js
@@ -2,34 +2,35 @@ $(document).on('ready', function() {
 
 	}
 
-function cambiarImagen(element) {
+function esconder_footer(element){
+	document.getElementsByClassName("contenedor__footer")['data'].visibility = hidden;
+}
 
-	var objTitulo = document.getElementsByClassName('Titulo-imagen')[0];
-	var objDescripcion = document.getElementsByClassName('Descripcion-imagen')[0];
+function cambiarImagen(element) {
 
     var puntos = document.getElementsByClassName('dots');
     for (var i = 0; i < puntos.length; ++i) {
-    var item = puntos[i];
-    item.src = 'img/empty_dot.png';
-}
+	    var item = puntos[i];
+	    item.src = 'images/empty_dot.png';
+		}
 
-    document.getElementsByClassName('dots')[parseInt(element.getAttribute('data'))-1].src= "img/Gray_dot.png";
+    document.getElementsByClassName('dots')[parseInt(element.getAttribute('data'))-1].src= "images/Gray_dot.png";
 
 
 	if(element.getAttribute('data') == "adelante") {
 		if(index <= 10) {
 			if(index == 10) index=0;
-			document.getElementsByClassName('Galeria-preview')[0].firstChild.src = "img/"+(index+1)+".jpg";
+			document.getElementsByClassName('portada')[0].firstChild.url = "images/"+(index+1)+".jpg";
 		}
 		index+=1;
 	} else if(element.getAttribute('data') == "atras") {
 		if(index >= 1) {
 			if(index==1) index=11;
-			document.getElementsByClassName('Galeria-preview')[0].firstChild.src = "img/"+(index-1)+".jpg";
+			document.getElementsByClassName('portada')[0].firstChild.url = "images/"+(index-1)+".jpg";
 		}
 		index-=1;
 	} else {
-		document.getElementsByClassName('Galeria-preview')[0].firstChild.src = "img/"+element.getAttribute('data')+".jpg";
+		document.getElementsByClassName('portada')[0].firstChild.url = "images/"+element.getAttribute('data')+".jpg";
 		index = parseInt(element.getAttribute('data'));
 	}
 	//Cambio de titulos y descripciones
