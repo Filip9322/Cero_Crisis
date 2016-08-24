d50d2ab6f92ce48b4f2398fb28e3c15750906c84
filip9322 <filip9322@gmail.com>
2016-08-24 02:04:56 -0500

    Se agrega una trasnparencia al header

--- build/java.js
+++ build/java.js
@@ -1,10 +1,8 @@
-$(document).on('ready', function() {
+$(document).on('ready', function()) {
 
 	}
 
-function esconder_footer(element){
-	document.getElementsByClassName("contenedor__footer")['data'].visibility = hidden;
-}
+
 
 function cambiarImagen(element) {
 
@@ -17,23 +15,4 @@ function cambiarImagen(element) {
     document.getElementsByClassName('dots')[parseInt(element.getAttribute('data'))-1].src= "images/Gray_dot.png";
 
 
-	if(element.getAttribute('data') == "adelante") {
-		if(index <= 10) {
-			if(index == 10) index=0;
-			document.getElementsByClassName('portada')[0].firstChild.url = "images/"+(index+1)+".jpg";
-		}
-		index+=1;
-	} else if(element.getAttribute('data') == "atras") {
-		if(index >= 1) {
-			if(index==1) index=11;
-			document.getElementsByClassName('portada')[0].firstChild.url = "images/"+(index-1)+".jpg";
-		}
-		index-=1;
-	} else {
-		document.getElementsByClassName('portada')[0].firstChild.url = "images/"+element.getAttribute('data')+".jpg";
-		index = parseInt(element.getAttribute('data'));
-	}
-	//Cambio de titulos y descripciones
-	objTitulo.innerHTML = textos.titulos[index-1];
-	objDescripcion.innerHTML = textos.descripciones[index-1];
 }
