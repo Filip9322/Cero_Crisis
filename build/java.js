8bc22dfc3e348e91019912a23fb311c5c3b2eba9
filip9322 <filip9322@gmail.com>
2016-08-24 02:38:37 -0500

    Se arregla el cambio de imagen por seleccion del dot

--- build/java.js
+++ build/java.js
@@ -1,18 +1,18 @@
-$(document).on('ready', function()) {
+$(document).on('ready', function() {
+	
+});
 
-	}
+	function cambiarImagen(element) {
 
-
-
-function cambiarImagen(element) {
-
-    var puntos = document.getElementsByClassName('dots');
-    for (var i = 0; i < puntos.length; ++i) {
-	    var item = puntos[i];
-	    item.src = 'images/empty_dot.png';
+		var puntos = document.getElementsByClassName('dots');
+		for (var i = 0; i < puntos.length; ++i) {
+			var item = puntos[i];
+			item.src = 'images/empty_dot.png';
 		}
 
-    document.getElementsByClassName('dots')[parseInt(element.getAttribute('data'))-1].src= "images/Gray_dot.png";
+		document.getElementsByClassName('dots')[parseInt(element.getAttribute('data'))-1].src= "images/Gray_dot.png";
 
+		index = parseInt(element.getAttribute('data'));
 
+		document.body.style.backgroundImage= "url('../images/"+element.getAttribute('data')+".jpg')";
 }
